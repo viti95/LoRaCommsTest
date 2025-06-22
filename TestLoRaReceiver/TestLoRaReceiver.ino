1253a00c9b11d371e600d49cb9b1a238ee9ee8b6
viti95 <victornt95@gmail.com>
2025-06-22 15:04:37 +0200

    Brightness control for rgb matrix

--- TestLoRaReceiver/TestLoRaReceiver.ino
+++ TestLoRaReceiver/TestLoRaReceiver.ino
@@ -7,7 +7,13 @@
 #define NUM_LEDS 64
 #define PIN_DATA_MATRIX 13
 #define OLED_BRIGHTNESS 255
-#define RGB_BRIGHTNESS 255 / 8
+#define RGB_BRIGHTNESS_HIGH 255
+#define RGB_BRIGHTNESS_MEDIUM 60
+#define RGB_BRIGHTNESS_LOW 10
+
+#define MESSAGE_BRILLO_BAJO "Brillo BAJO"
+#define MESSAGE_BRILLO_MEDIO "Brillo MEDIO"
+#define MESSAGE_BRILLO_ALTO "Brillo ALTO"
 
 #define MESSAGE_BOX "BOX"
 #define MESSAGE_SLOW "SLOW"
@@ -16,11 +22,17 @@
 #define MESSAGE_DRIVE_THROUGH "DRV TR"
 #define MESSAGE_BLOCK "BLOCK"
 
+int buttonState = 0;  // variable for reading the pushbutton status
+int brightness = 2;
+
 CRGBArray<NUM_LEDS> leds;
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, OLED_RESET, OLED_CLOCK, OLED_DATA);
 
 void setup() {
+
+  pinMode(PRG_BUTTON, INPUT);
+
   Serial.begin(115200);
   while (!Serial)
     ;
@@ -28,7 +40,7 @@ void setup() {
   Serial.println("Init WS2812 matrix led");
 
   FastLED.addLeds<NEOPIXEL, PIN_DATA_MATRIX>(leds, NUM_LEDS);
-  FastLED.setBrightness(RGB_BRIGHTNESS);
+  FastLED.setBrightness(RGB_BRIGHTNESS_HIGH);
 
   Serial.println("Init SSD1306 display");
 
@@ -201,7 +213,49 @@ void cmd_block() {
 
 }
 
+void cmd_cambio_brillo() {
+  
+  brightness++;
+
+  if (brightness == 3)
+    brightness = 0;
+  
+  u8g2.setFont(u8g2_font_logisoso18_tr);
+  u8g2.clear();
+  u8g2.setCursor(0, get_y_cursor());
+
+  switch(brightness){
+    case 0:
+      u8g2.print(MESSAGE_BRILLO_BAJO);
+      FastLED.setBrightness(RGB_BRIGHTNESS_LOW);  
+      break;
+    case 1:
+      u8g2.print(MESSAGE_BRILLO_MEDIO);
+      FastLED.setBrightness(RGB_BRIGHTNESS_MEDIUM);
+      break;
+    case 2:
+      u8g2.print(MESSAGE_BRILLO_ALTO);
+      FastLED.setBrightness(RGB_BRIGHTNESS_HIGH);
+      break;
+  }
+
+  u8g2.sendBuffer();
+
+  delay(1000);
+
+  u8g2.clear();
+  u8g2.sendBuffer();
+}
+
 void loop() {
+
+  buttonState = digitalRead(PRG_BUTTON);
+
+  // read button
+  if (buttonState == LOW) { // WTF
+    cmd_cambio_brillo();
+  }
+
   // try to parse packet
   int packetSize = LoRa.parsePacket();
 
